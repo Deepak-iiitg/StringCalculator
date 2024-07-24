8f886c90a3c88e586192baef915406b983d6d993
Deepak-iiitg <deepak.k@iiitg.ac.in>
2024-07-24 23:18:27 +0530

    Throw error for negative number

--- Calculator.js
+++ Calculator.js
@@ -8,16 +8,25 @@ function Add(numbers) {
         let delimeter = delimiterLine.substring(2);
         delimiterArray.push(delimeter);
         numbers = numbers.substring(numbers.indexOf("\n") + 1); // Exclude the delimiter line
+    }else{
+        delimiterArray.push(',');
     }
     //split the numbers with given delimeter
     const nums = numbers.split(new RegExp(`[${delimiterArray}\n]`));
     let sum = 0;
-    
+    let negatives = [];
     for (let num of nums) {
         if (num.trim() !== '') {
             const n = parseInt(num);
-            sum += n;
+            if(n<0){
+                negatives.push(n);
+            }else{
+               sum += n;
+            }
         }
+    }
+    if (negatives.length > 0) {
+        throw new Error("negatives not allowed: " + negatives.join(", "));
     }
     return sum;
 }
@@ -37,3 +46,6 @@ console.log(Add("1\n2,3\n5"));
 
 //(4) Test different delimeter
 console.log(Add("//;\n1;2"));
+
+//(5) Test for negative number to throw error
+console.log(Add("-10,-19,20,30,-40"));

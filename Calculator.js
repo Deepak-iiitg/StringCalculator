4505892b79165ba8c41a442ba625b75edadc73ed
Deepak-iiitg <deepak.k@iiitg.ac.in>
2024-07-24 23:13:08 +0530

    support different delimeter like //[delimiter]\n[numbers…] this

--- Calculator.js
+++ Calculator.js
@@ -3,9 +3,13 @@ function Add(numbers) {
         return 0;
     }
     let delimiterArray = [];
-    let delimeter = ',';
-    delimiterArray.push(delimeter);
-    //split the numbers with given delimeter \n and comma
+    if (numbers.startsWith("//")) {
+        const delimiterLine = numbers.split("\n")[0];
+        let delimeter = delimiterLine.substring(2);
+        delimiterArray.push(delimeter);
+        numbers = numbers.substring(numbers.indexOf("\n") + 1); // Exclude the delimiter line
+    }
+    //split the numbers with given delimeter
     const nums = numbers.split(new RegExp(`[${delimiterArray}\n]`));
     let sum = 0;
     
@@ -30,3 +34,6 @@ console.log(Add("10,20,1,9,1,1,1,13"));
 //(3) Test case for delimeter \n and comma
 console.log(Add("1\n2,3"));
 console.log(Add("1\n2,3\n5"));
+
+//(4) Test different delimeter
+console.log(Add("//;\n1;2"));

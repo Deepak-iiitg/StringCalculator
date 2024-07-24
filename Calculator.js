97aa5727df15f4e5ec67aadc538f1f7b4768798f
Deepak-iiitg <deepak.k@iiitg.ac.in>
2024-07-24 23:07:11 +0530

    Handled delimeter \n and comma

--- Calculator.js
+++ Calculator.js
@@ -5,8 +5,8 @@ function Add(numbers) {
     let delimiterArray = [];
     let delimeter = ',';
     delimiterArray.push(delimeter);
-    //split the numbers with given delimeter
-    const nums = numbers.split(new RegExp(`[${delimiterArray}]`));
+    //split the numbers with given delimeter \n and comma
+    const nums = numbers.split(new RegExp(`[${delimiterArray}\n]`));
     let sum = 0;
     
     for (let num of nums) {
@@ -26,3 +26,7 @@ console.log(Add("1,2"));
 //(2) Test Known amount of numbers
 console.log(Add("1,2,3,10,12,188"));
 console.log(Add("10,20,1,9,1,1,1,13"));
+
+//(3) Test case for delimeter \n and comma
+console.log(Add("1\n2,3"));
+console.log(Add("1\n2,3\n5"));
